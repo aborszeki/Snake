90f0a0e5fa5f751d6a5b02ae5dcbe9d76bcf3f1c
aborszeki <aborszeki@gmail.com>
2015-05-15 10:40:40 +0200

    Snake first.

---
+++ Snake/Snake/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+
+// TARGET - SKELETON
+// v1
+// - Step()
+// - Turn(direction)
+// - Reset
+// Components
+// Engine
+// Model
+// --> Snake
+// --> Board
+namespace Snake
+{
+    public enum Direction
+    {
+        Left, Right, Top, Bottom
+    }
+
+    public enum StepResult
+    {
+        SnakeDied, Survived, LevelComplete
+    }
+
+    public enum CellType
+    {
+        Wall, Empty, Food, Snake, SnakeHead
+    }
+
+    public interface ISnake
+    {
+        Direction Direction { get; set; }
+        StepResult Step();
+        void Add(Point point);
+    }
+
+    public interface IBoard
+    {
+        void Build(int width, int height);
+        CellType this[int i, int j] { get; set; }
+        void AddFood();
+    }
+
+    public interface IGame
+    {
+        StepResult Step();
+        void KeyPress(ConsoleKey key);
+        void Start(IBoard board, ISnake snake);
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Snake/UnitTest/IntegrationTests.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using Snake;
+
+namespace UnitTest
+{
+    [TestFixture]
+    public class IntegrationTests
+    {
+        [Test]
+        public void Snake_Moves_1_Step_Right()
+        {
+
+            string before = @"
+                +++++++++
+                +       +
+                +  @@*  +
+                +       +
+                +++++++++";
+
+            string expected = @"
+                +++++++++
+                +       +
+                +   @@* +
+                +       +
+                +++++++++";
+
+            IGame game;
+            IBoard board = BuildBoardFromString(before);
+            ISnake snake = BuildSnakeFromString(before);
+            snake.Direction = Direction.Right;
+
+            game.Start(board, snake);
+            game.Step();
+
+            Assert.That(board.ToString(), Is.EqualTo(expected));
+        }
+
+        private IBoard BuildBoardFromString(string sBoard)
+        {
+            IBoard result;
+            var lines = sBoard.Split('\n');
+            var height = lines.Length;
+            var width = lines.Max(x => x.Length);
+
+            for (int j = 0; j < height; j++)
+            {
+                for (int i = 0; i < width; i++)
+                {
+                    result[i, j] = CellTypeFromChar(lines[j][i]);
+                }
+            }
+        }
+
+        private Dictionary<char, CellType> cellTypeConverterDictionary_ = new Dictionary<char, CellType>()
+        {
+            {'+', CellType.Wall},
+            {'@', CellType.Snake},
+            {'*', CellType.SnakeHead},
+            {'F', CellType.Food}
+        };
+
+        private CellType CellTypeFromChar(char ch)
+        {
+            CellType result = CellType.Empty;
+            cellTypeConverterDictionary_.TryGetValue(ch, out result);
+            return result;
+        }
+
+        private ISnake BuildSnakeFromString(string sBoard)
+        {
+
+        }
+
+    }
+
+}
